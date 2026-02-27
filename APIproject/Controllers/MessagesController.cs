32a88164c9d920f1244ae7aca0fa250f779eefd2
sena <84151030+padfootss@users.noreply.github.com>
2026-02-27 22:09:22 +0300

    Add comments to MessagesController methods
    Added comments to clarify the SendMessage and GetMessages methods.

--- APIproject/Controllers/MessagesController.cs
+++ APIproject/Controllers/MessagesController.cs
@@ -16,17 +16,18 @@ namespace APIproject.Controllers
             _context = context;
         }
 
-        [HttpPost]
+        [HttpPost] // istek geliyor veri göndermek için
         public IActionResult SendMessage(Message message)
         {
-            _context.Messages.Add(message);
-            _context.SaveChanges();
-            return Ok(message);
+            _context.Messages.Add(message);  //mesaj gönderiyor
+            _context.SaveChanges(); // database kaydediyor
+            return Ok(message); // http response 200 döndürür 
         }
 
-        [HttpGet("{userId}")]
+        [HttpGet("{userId}")] // istek veri alma
         public IActionResult GetMessages(int userId)
-        {
+        {    
+        //database sorguuyor
             var messages = _context.Messages
                 .Include(m => m.Sender)
                 .Include(m => m.Receiver)
